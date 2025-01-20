e0f358163b416d4f4aa691ed4e1e12b45bc7cf44
flutewizard100 <dorinamevans@gmail.com>
2025-01-20 08:27:56 -0500

    this is code that almost works for the keyholes

--- src/keyholesBobally.py
+++ src/keyholesBobally.py
@@ -5,29 +5,26 @@ drone.pair()
 # drone.load_color_data("color_data")
 height_for_yellow = 90
 
-# for i in range(100):
-#
-#     color_data = drone.get_color_data()
-#
-#     color = drone.predict_colors(color_data)
-#
-# print(color)
-#
-#
-#
-# if color == ["red", "red"]:
-#     drone.set_drone_LED(255, 0, 0, 100)  # Red
-#     drone.set_controller_LED(255, 0, 0, 100)  # Red
-# elif color == ["green", "green"]:
-#     drone.set_drone_LED(0, 255, 0, 100)  # Green
-#     drone.set_controller_LED(0, 255, 0, 100)  # Green
-# elif color == ["blue", "blue"]:
-#     drone.set_drone_LED(0, 0, 255, 100)  # Blue
-#     drone.set_controller_LED(0, 0, 255, 100)  # Blue
-#
-#
-#
-# drone.calibrate()
+for i in range(500):
+    color_data = drone.get_color_data()
+    color = drone.predict_colors(color_data)
+    print(color)
+
+print(color)
+
+if color == ["red", "red"]:
+    drone.set_drone_LED(255, 0, 0, 100)  # Red
+    drone.set_controller_LED(255, 0, 0, 100)  # Red
+elif color == ["green", "green"]:
+    drone.set_drone_LED(0, 255, 0, 100)  # Green
+    drone.set_controller_LED(0, 255, 0, 100)  # Green
+elif color == ["blue", "blue"]:
+    drone.set_drone_LED(0, 0, 255, 100)  # Blue
+    drone.set_controller_LED(0, 0, 255, 100)  # Blue
+
+
+
+#drone.calibrate()
 drone.takeoff()
 height = drone.get_height()
 print(height)
@@ -48,34 +45,35 @@ while height_for_yellow + 3 < height or height < height_for_yellow - 3:
 
 print("Correct height, moving forward!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 drone.set_throttle(50)
+drone.move(2)
+drone.set_throttle(0)
 drone.set_pitch(80)
-drone.move(1.45)
+drone.move(2.1)
 print("I'm Forward")
 drone.set_pitch(0)
 drone.set_throttle(-50)
-drone.move(0.8)
-print("I'm Down")
 drone.move(0.5)
+print("I'm Down")
 print("I'm Down!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 drone.set_throttle(0)
 drone.set_pitch(0)
-drone.set_roll(80)
-drone.move(1.2)
+##drone.set_roll(60)
+drone.move(0.3)
 print("I'm Right")
 drone.set_throttle(0)
 drone.set_pitch(0)
-drone.set_roll(-80)
-drone.move(0.5)
+#drone.set_roll(-80)
+#drone.move(0.5)
 print("I'm Right")
 drone.set_throttle(0)
 drone.set_pitch(0)
 drone.set_roll(50)
-drone.move(3)
-drone.set_roll(80)
-drone.move(1.2)
-print("I'm Right")
-drone.set_pitch(-10)
+drone.move(2)
+# drone.set_roll(80)
+# drone.move(1.2)
+# print("I'm Right")
+drone.set_pitch(-20)
 drone.move(1)
 print("I'm landing")
-drone.spiral(50, 5, 1)
+#drone.spiral(50, 5, 1)
 drone.land()
